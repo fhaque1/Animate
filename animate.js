927a1d04d0547345939063dffb5a7f73fc91a99c
fhaque1 <farhan3231@gmail.com>
2017-02-18 13:29:33 -0500

    grow shrink finished

--- animate.js
+++ animate.js
@@ -2,27 +2,12 @@ var c = document.getElementById("slate");
 var ctx = c.getContext('2d');
 ctx.fillStyle = "blue";
 
-
 var rid = 0;
 
 var animateDot = function(){
 	var x = 0;
 	window.cancelAnimationFrame(rid);
-	var growDot = function() {
-		ctx.clearRect(0, 0, c.width, c.height);
-		console.log(rid);	
-		ctx.beginPath();
-		ctx.arc(c.width/2, c.height/2, x, 0, 2*Math.PI);    
-		ctx.fill();
-		x++;
-		rid = window.requestAnimationFrame(growDot);
-	};
-	growDot();
-};
-
-var animateDot2 = function(){
-	var x = 250;
-	window.cancelAnimationFrame(rid);
+	var a = Math.min(c.height/2, c.width/2);
 	var shrinkDot = function() {
 		ctx.clearRect(0, 0, c.width, c.height);
 		console.log(rid);	
@@ -30,12 +15,22 @@ var animateDot2 = function(){
 		ctx.arc(c.width/2, c.height/2, x, 0, 2*Math.PI);    
 		ctx.fill();
 		x--;
+		if (x <= 0)
+			return growDot();
 		rid = window.requestAnimationFrame(shrinkDot);
 	};
-	shrinkDot();
-};
-
-var growShrink = function() {
+	var growDot = function() {
+		ctx.clearRect(0, 0, c.width, c.height);
+		console.log(rid);	
+		ctx.beginPath();
+		ctx.arc(c.width/2, c.height/2, x, 0, 2*Math.PI);    
+		ctx.fill();
+		x++;
+		if (x > a)
+			return shrinkDot();
+		rid = window.requestAnimationFrame(growDot);
+	};
+	growDot();
 };
 
 var stopIt = function() {
